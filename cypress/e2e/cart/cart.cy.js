1ae281cf212620abcd16eeaae0ee75e7c5b25110
gabrielVolponi <0gabrielvolponi0@gmail.com>
2025-10-08 16:15:10 -0300

    cart spec

--- cypress/e2e/cart/cart.cy.js
+++ cypress/e2e/cart/cart.cy.js
@@ -1,14 +1,29 @@
 /// <reference types="cypress" />
 
 import Cart from "../../pages/cart";
+import inventory from "../../pages/inventory";
+import Inventory from "../../pages/inventory";
 
-describe('Cart', () => {
-	beforeEach(() => {
-		Cart.visitPage();
-        Cart.checkUrl();
-	});
+describe("Cart", () => {
+  beforeEach(() => {
+    Cart.visitPage();
+    Cart.checkUrl();
+  });
 
-	it('deve testar o carrinho', () => {
-		expect(true).eq(true)
-	});
+  it("Visit Cart Page", () => {
+    Cart.visitPage();
+    Cart.checkUrl();
+  });
+
+  it("Back to Shopping", () => {
+    Cart.backToShopping();
+    Inventory.checkUrl();
+  });
+
+  it("Should remove item from cart", () => {
+    Inventory.visitPage();
+    Inventory.addProductToCartById(4);
+    Inventory.acessCart();
+    cy.contains("REMOVE").should("be.visible").click();
+  });
 });
