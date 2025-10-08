9b5d9f3fe17cbe411cecf685526f436246645809
gabrielVolponi <0gabrielvolponi0@gmail.com>
2025-10-08 16:15:25 -0300

    screenshot on failure

--- cypress/pages/cart/index.js
+++ cypress/pages/cart/index.js
@@ -3,10 +3,26 @@ const baseUrl = Cypress.config("baseUrl");
 class Cart {
   visitPage() {
     cy.visit(`${baseUrl}/cart.html`);
+    Cypress.on('fail', (error, runnable) => {
+      cy.screenshot('failure_visitPage');
+      throw error;
+    });
   }
 
   checkUrl() {
     cy.url().should("contains", `${baseUrl}/cart.html`);
+    Cypress.on('fail', (error, runnable) => {
+      cy.screenshot('failure_checkUrl');
+      throw error;
+    });
+  }
+
+  backToShopping(){
+    cy.contains('Continue Shopping').should('be.visible').click()
+    Cypress.on('fail', (error, runnable) => {
+      cy.screenshot('failure_backToShopping');
+      throw error;
+    });
   }
 }
 
